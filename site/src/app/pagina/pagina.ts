92adb2783c5ba69f47e8fee5bf29f172968c570f
Gu3001 <gustavo180g@gmail.com>
2020-02-13 22:39:49 -0300

    Primeira aula de angular

---
+++ site/src/app/pagina/pagina.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "app-pagina",
+  templateUrl: "./pagina.html",
+  stylesUrls: "./pagina.css"
+})
+
+export class Pagina{
+  title="site";
+}
