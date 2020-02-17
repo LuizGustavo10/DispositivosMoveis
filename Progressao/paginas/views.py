3a44a88c9cd9fb5d5fe3c9631737c1c577fa15b9
Gu3001 <gustavo180g@gmail.com>
2020-02-17 20:44:27 -0300

    adicionado js css

---
+++ Progressao/paginas/views.py
@@ -0,0 +1,18 @@
+
+# Importar o TemplateView para criar páginas simples
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+# A classe PaginaInicial "extends" TemplateView
+class PaginaInicial(TemplateView):
+    # Toda classe filha do TemplateView precisa do
+    # atributo abaixo para definir um template a ser renderizado
+    template_name = 'paginas/index.html'
+
+
+class Sobre(TemplateView):
+    # Toda classe filha do TemplateView precisa do
+    # atributo abaixo para definir um template a ser renderizado
+    template_name = 'paginas/sobre.html'
